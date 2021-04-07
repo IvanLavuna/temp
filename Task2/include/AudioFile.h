89de7206b73c94cecddec5e7ac3ea5b7561d81a7
Ivan Manchur <ivan.manchur47@gmail.com>
2021-04-07 16:35:56 +0300

    AudioFile implementation

--- Task2/include/AudioFile.h
+++ Task2/include/AudioFile.h
@@ -6,18 +6,57 @@
 #define MEDIAPLAYERAPP_AUDIOFILE_H
 
 
+typedef unsigned int uint;
+
 class AudioFile	: public QObject
 {
 Q_OBJECT
 private:
-
-
+	QString  m_filePath{};
+	QString  m_artist{};
+	QString  m_title{};
+	QString  m_album{};
+	QString  m_genre{};
+	uint     m_year{};
 private slots:
 
 protected:
 
 public:
-	explicit AudioFile(){};
+	/// constructors / destructor
+	AudioFile();
+	explicit AudioFile(QString& sourcePath);
+	explicit AudioFile(QString&& sourcePath);
+	~AudioFile() override = default;
+
+	/// get
+	QString getFilePath() const noexcept;
+	QString getArtist()   const noexcept;
+	QString getTitle()    const noexcept;
+	QString getAlbum()    const noexcept;
+	QString getGenre()    const noexcept;
+	uint    getYear()     const noexcept;
+
+	/// set
+	void setFilePath(QString& filePath);
+	void setFilePath(QString&& filePath);
+
+	void setArtist(QString& artist);
+	void setArtist(QString&& artist);
+
+	void setTitle(QString& title);
+	void setTitle(QString&& title);
+
+	void setAlbum(QString& album);
+	void setAlbum(QString&& album);
+
+	void setGenre(QString& genre);
+	void setGenre(QString&& genre);
+
+	void setYear(uint year);
+
+	void setAudio(QString& sourcePath);
+	void setAudio(QString&& sourcePath);
 
 signals:
 };
--- Task2/source/AudioFile.cpp
+++ Task2/source/AudioFile.cpp
@@ -3,3 +3,149 @@
 //
 
 #include "AudioFile.h"
+
+/// constructors
+AudioFile::AudioFile() = default;
+
+AudioFile::AudioFile(QString &sourcePath)
+{
+	TagLib::FileRef f(sourcePath.toStdString().c_str());
+	if(f.isNull())
+		throw ERROR_MESSAGE("Failed to open." );
+
+	m_filePath = sourcePath;
+	m_artist   = f.tag()->artist().toCString();
+	m_title    = f.tag()->title().toCString();
+	m_album    = f.tag()->album().toCString();
+	m_genre    = f.tag()->genre().toCString();
+	m_year     = f.tag()->year();
+}
+
+AudioFile::AudioFile(QString &&sourcePath)
+{
+	TagLib::FileRef f(sourcePath.toStdString().c_str());
+	if(f.isNull())
+		throw ERROR_MESSAGE("Failed to open." );
+
+	m_filePath = sourcePath;
+	m_artist   = f.tag()->artist().toCString();
+	m_title    = f.tag()->title().toCString();
+	m_album    = f.tag()->album().toCString();
+	m_genre    = f.tag()->genre().toCString();
+	m_year     = f.tag()->year();
+}
+
+/// get
+QString AudioFile::getFilePath() const noexcept
+{
+	return m_filePath;
+}
+
+QString AudioFile::getArtist() const noexcept
+{
+	return m_artist;
+}
+
+QString AudioFile::getTitle() const noexcept
+{
+	return m_title;
+}
+
+QString AudioFile::getAlbum() const noexcept
+{
+	return m_album;
+}
+
+QString AudioFile::getGenre() const noexcept
+{
+	return m_genre;
+}
+
+uint AudioFile::getYear() const noexcept
+{
+	return m_year;
+}
+
+/// set
+void AudioFile::setFilePath(QString &filePath)
+{
+	m_filePath = filePath;
+}
+void AudioFile::setFilePath(QString &&filePath)
+{
+	m_filePath = filePath;
+
+}
+
+void AudioFile::setArtist(QString &artist)
+{
+	m_artist = artist;
+}
+
+void AudioFile::setArtist(QString &&artist)
+{
+	m_artist = artist;
+}
+
+void AudioFile::setTitle(QString &title)
+{
+	m_title = title;
+}
+
+void AudioFile::setTitle(QString &&title)
+{
+	m_title = title;
+}
+
+void AudioFile::setAlbum(QString &album)
+{
+	m_album = album;
+}
+
+void AudioFile::setAlbum(QString &&album)
+{
+	m_album = album;
+}
+
+void AudioFile::setGenre(QString &genre)
+{
+	m_genre = genre;
+}
+
+void AudioFile::setGenre(QString &&genre)
+{
+	m_genre = genre;
+}
+
+void AudioFile::setYear(uint year)
+{
+	m_year = year;
+}
+
+void AudioFile::setAudio(QString &sourcePath)
+{
+	TagLib::FileRef f(sourcePath.toStdString().c_str());
+	if(f.isNull())
+		throw ERROR_MESSAGE("Failed to open." );
+
+	m_filePath = sourcePath;
+	m_artist   = f.tag()->artist().toCString();
+	m_title    = f.tag()->title().toCString();
+	m_album    = f.tag()->album().toCString();
+	m_genre    = f.tag()->genre().toCString();
+	m_year     = f.tag()->year();
+}
+
+void AudioFile::setAudio(QString &&sourcePath)
+{
+	TagLib::FileRef f(sourcePath.toStdString().c_str());
+	if(f.isNull())
+		throw ERROR_MESSAGE("Failed to open." );
+
+	m_filePath = sourcePath;
+	m_artist   = f.tag()->artist().toCString();
+	m_title    = f.tag()->title().toCString();
+	m_album    = f.tag()->album().toCString();
+	m_genre    = f.tag()->genre().toCString();
+	m_year     = f.tag()->year();
+}
